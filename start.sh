0ead7f7a19ccb1c55633f3b65be66f633f0ad185
Nadid <emamulislamnadid_cse30d@portcity.edu.bd>
2026-08-10 03:57:50 +0600

    fix: use direct path for uvicorn

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export PATH="/opt/render/project/python/bin:$PATH"
-uvicorn main:app --host 0.0.0.0 --port $PORT
+/opt/render/project/python/bin/uvicorn main:app --host 0.0.0.0 --port $PORT
